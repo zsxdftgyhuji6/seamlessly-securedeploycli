09ad7e7ac68a725d0181430ef7207d4c2c9c2e1d
zsxdftgyhuji6 <gs1shayel22@hotmail.com>
2024-05-07 10:37:13 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const isPalindrome = (str) => str === str.split("").reverse().join("");
+const binarySearchRecursive = (
+  arr,
+  target,
+  left = 0,
+  right = arr.length - 1,
+) => {
+  if (left > right) {
+    return -1;
+  }
+  const mid = Math.floor((left + right) / 2);
+  if (arr[mid] === target) {
+    return mid;
+  } else if (arr[mid] < target) {
+    return binarySearchRecursive(arr, target, mid + 1, right);
+  } else {
+    return binarySearchRecursive(arr, target, left, mid - 1);
+  }
+};
