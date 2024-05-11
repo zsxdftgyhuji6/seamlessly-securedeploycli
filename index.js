e979cf04c272080959368819d7e76a48d76fd839
zsxdftgyhuji6 <gs1shayel22@hotmail.com>
2024-05-12 00:08:23 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-function linearSearch(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) return i;
-  }
-  return -1;
-}
+const flatten = (arr) => arr.reduce((a, v) => a.concat(v), []);
