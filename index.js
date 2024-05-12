2c008002eaf76ac00b4de75690c8eb9024941bc4
zsxdftgyhuji6 <gs1shayel22@hotmail.com>
2024-05-13 01:54:37 +0700

    update project commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const flatten = (arr) => arr.reduce((a, v) => a.concat(v), []);
+function coinChange(coins, amount) {
+  const dp = new Array(amount + 1).fill(amount + 1);
+  dp[0] = 0;
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+    }
+  }
+  return dp[amount] > amount ? -1 : dp[amount];
+}
